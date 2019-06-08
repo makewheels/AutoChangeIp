a51875e7666535c3340a823b36c87e19cb44914c
Administrator <user@email.com>
2019-06-08 22:08:03 +0800

    发邮件

--- src/main/java/start/Run.java
+++ src/main/java/start/Run.java
@@ -5,6 +5,7 @@ import com.alibaba.fastjson.JSON;
 import util.HttpUtil;
 import util.QcloudCosUtil;
 import util.SsrUtil;
+import util.TimeUtil;
 
 import java.io.File;
 import java.io.FileOutputStream;
@@ -51,35 +52,50 @@ public class Run {
     }
 
     public static void main(String[] args) {
+        System.out.println("----------");
+        System.out.println("开始：" + TimeUtil.getTime());
+        System.out.println("正在获取当前ip....");
         //先拿到本机ip
         String oldIp = getIp();
+        System.out.println("老ip是：" + oldIp);
         //检查是否被墙
         boolean canUse = checkIp(oldIp);
-        //如果被墙了
-        if (canUse == false) {
-            //换ip
-            changeIp();
-            //再次查询ip
-            String newIp = getIp();
-            //再次查询是否被墙
-            if (checkIp(newIp) == false) {
-                System.out.println("your father 还是被墙");
-            }
-            //更新ssr订阅链接
-            String ssrLink = SsrUtil.getSsrLink(newIp, "12000", "1wga0gar564gw00w4e",
-                    "台湾", "谷歌云");
-            //写到本地文件
-            File file = new File("ssrsub");
-            try {
-                FileOutputStream fileOutputStream = new FileOutputStream(file);
-                fileOutputStream.write(ssrLink.getBytes());
-                fileOutputStream.flush();
-                fileOutputStream.close();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            //上传到腾讯云对象存储
-            QcloudCosUtil.saveToQcloud(file, "/subscription/wR2ez5pc4ISN4BIJqczWhvSMIfngJUACHr");
+        System.out.println("已完成检测是否被墙");
+        //如果还能用
+        if (canUse == true) {
+            System.out.println("还能用，没被墙");
+            return;
         }
+        System.out.println("被墙了。。开始执行换ip程序");
+        //如果被墙了
+        //换ip
+        System.out.println("开始换ip");
+        changeIp();
+        System.out.println("换ip已完成");
+        //再次查询ip
+        String newIp = getIp();
+        System.out.println("新ip是：" + newIp);
+        System.out.println("再次检测是否被墙.....");
+        //再次查询是否被墙
+        if (checkIp(newIp) == false) {
+            System.out.println("your father 还是被墙");
+        }
+        System.out.println("再次检测完成，没墙，换ip成功！");
+        System.out.println("开始更新ssr订阅文件");
+        //更新ssr订阅链接
+        String ssrLink = SsrUtil.getSsrLink(newIp, "12000", "1wga0gar564gw00w4e",
+                "台湾", "谷歌云");
+        //写到本地文件
+        File file = new File("ssrsub");
+        try {
+            FileOutputStream fileOutputStream = new FileOutputStream(file);
+            fileOutputStream.write(ssrLink.getBytes());
+            fileOutputStream.flush();
+            fileOutputStream.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        //上传到腾讯云对象存储
+        QcloudCosUtil.saveToQcloud(file, "/subscription/wR2ez5pc4ISN4BIJqczWhvSMIfngJUACHr");
     }
 }
---
+++ src/main/java/util/MailUtil.java
@@ -0,0 +1,39 @@
+package util;
+
+import javax.mail.*;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import java.util.Properties;
+
+/**
+ * 发邮件工具类
+ */
+public class MailUtil {
+    public static void send(){
+        Properties props = new Properties();
+        props.put("mail.smtp.host", "smtp.gmail.com");
+        props.put("mail.smtp.socketFactory.port", "465");
+        props.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.port", "465");
+        Session session = Session.getDefaultInstance(props,
+                new javax.mail.Authenticator() {
+                    protected PasswordAuthentication getPasswordAuthentication() {
+                        return new PasswordAuthentication("username","password");
+                    }
+                });
+        try {
+            Message message = new MimeMessage(session);
+            message.setFrom(new InternetAddress("from@no-spam.com"));
+            message.setRecipients(Message.RecipientType.TO,
+                    InternetAddress.parse("to@no-spam.com"));
+            message.setSubject("Testing Subject");
+            message.setText("Dear Mail Crawler," +
+                    "\n\n No spam to my email, please!");
+            Transport.send(message);
+            System.out.println("Done");
+        } catch (MessagingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
